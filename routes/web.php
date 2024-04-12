ab8196be60e3535b8bf35407a413a7ec5396a39c
Gevorg Melkumyan <gevorgmelkoumyan@gmail.com>
2024-04-12 23:19:30 +0400

    Installed league/flysystem-aws-s3-v3, added a new docker service for s3mock

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Mail\TestEmail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,4 +14,28 @@ Route::any('send-test-email', function (Request $request) {
     Mail::to($request->email)->queue(new TestEmail);
 
     return response()->json();
+});
+
+Route::any('list-s3-bucket', function () {
+   $files = Storage::allFiles();
+
+   return response()->json(compact('files'));
+});
+
+Route::any('store-in-s3', function (Request $request) {
+    $file = file_get_contents(public_path('test.pdf'));
+    $name = ($request->name ?? 'test') . '.pdf';
+
+    $success = Storage::put($name, $file);
+
+    if ($success) {
+        return response()->json([
+            'url' => Storage::url($name),
+            'temporaryUrl' => Storage::temporaryUrl($name, now()->addMinute()),
+        ]);
+    }
+
+    return response()->json([
+        'error' => 'Failed to store test file',
+    ], 500);
 });
